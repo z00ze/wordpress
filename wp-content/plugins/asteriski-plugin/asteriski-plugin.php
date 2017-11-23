ea1e98e9b8840d7e601032a0b653137f142ecd87
Marko Loponen <majulop@utu.fi>
2017-11-23 16:02:10 +0200

    Asteriski Plugin
    init

---
+++ wp-content/plugins/asteriski-plugin/asteriski-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Asteriski Plugin
+* Plugin URI: http://www.asteriski.fi
+* Description: Asteriski-lisäosa Wordpressiin. Artikkelien lähetys ja niiden hallinnointi.
+* Version: 0.0.1
+* Author: Marko Loponen
+* Author URI: https://github.com/z00ze
+* License: GGWP 666
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+?>
