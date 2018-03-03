77eab61f5fe34b338e175280c088b84216158fcb
Marko Loponen <majulop@utu.fi>
2018-03-03 14:05:01 +0200

    Added Facebook og
    To handle the thumbnail images properly when posting to facebook.

--- wp-content/themes/travelify-child/functions.php
+++ wp-content/themes/travelify-child/functions.php
@@ -51,4 +51,34 @@ function new_mail_from_name($old) {
 return 'Riski-info';
 }
 
+/* Facebook og */
+//Adding the Open Graph in the Language Attributes
+function add_opengraph_doctype( $output ) {
+        return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+    }
+add_filter('language_attributes', 'add_opengraph_doctype');
+ 
+//Lets add Open Graph Meta Info
+ 
+function insert_fb_in_head() {
+    global $post;
+    if ( !is_singular()) return;
+        echo '<meta property="fb:admins" content="1706607593"/>';
+        echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+        echo '<meta property="og:type" content="article"/>';
+        echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+        echo '<meta property="og:site_name" content="https://www.asteriski.fi"/>';
+        echo '<meta property="og:description" content="Asteriski ry - Turun tietojenkäsittelytieteiden opiskelijoiden ainejärjestö."/>';
+        echo '<meta property="fb:app_id" content="900688140104027"/>';
+    if(!has_post_thumbnail( $post->ID )) { 
+        $default_image="http://asteriski.utu.fi/wp-content/uploads/2017/09/asteriski_logo_netisivu.png";
+        echo '<meta property="og:image" content="' . $default_image . '"/>';
+    }
+    else{
+        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+    }
+    echo "";
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
 ?>
